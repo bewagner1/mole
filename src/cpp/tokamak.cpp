65fa20c38a370adbf8fe8009324d15ae3ebc2ac6
Ben Wagner <benjaminwagner21@gmail.com>
2026-04-08 19:12:59 -0700

    some implementation

--- src/cpp/tokamak.cpp
+++ src/cpp/tokamak.cpp
@@ -1,23 +1,140 @@
 /**
+ * @file tokamak.cpp
  * 
+ * @brief Implementation of functions for solving the Grad-Shafranov equation
  */
 
 #include "tokamak.h"
 
-// 
-sp_mat weighted_laplacian()
+// Used for reference grid
+sp_mat weighted_laplacian(u16 k, u32 m, Real dx, u32 n, Real dy, Real alpha, Real beta)
 {
+    Divergence De(k, m, dx);
+    Gradient Ge(k, m, dx);
 
+    Divergence Dn(k, n, dy);
+    Gradient Gn(k, n, dy);
+
+    sp_mat Im = speye(m + 2, m + 2);
+    Im.shed_col(0);
+    Im.shed_col(m);
+
+    sp_mat In = speye(n + 2, n + 2);
+    In.shed_col(0);
+    In.shed_col(m);
+
+    sp_mat Dx = Utils::spkron(In, De);
+    sp_mat Gx = Utils::spkron(In.t(), Ge);
+    sp_mat Dy = Utils::spkron(Dn, Im);
+    sp_mat Gy = Utils::spkron(Gn, Im.t());
+
+    sp_mat A = (1.0 / (alpha * alpha)) * speye(Dx.n_cols, Gx.n_rows);
+    sp_mat B = (1.0 / (beta * beta)) * speye(Dy.n_cols, Gy.n_rows);
+
+    sp_mat L = Dx * A * Gx + Dy * B * Gy;
+    return L;
 }
 
-// 
-vec reference_grid()
+// Generates x- or y- coordinates of plasma grid
+vec plasma_grid(u16 k, vec left, vec right, vec bottom, vec top)
 {
+    u32 m = bottom.n_elem - 1;
+    u32 n = top.n_elem - 1;
 
+    Real dx = 1.0 / m;
+    Real dy = 1.0 / n;
+
+    Real alpha = 0.0;
+    Real beta = 0.0;
+
+    ivec dc = {1, 1, 1, 1};
+    ivec nc = {0, 0, 0, 0};
+
+    InterpolNtoC INC(k, m, n, dc, nc);
+    InterpolCtoN ICN(k, m, n, dc, nc);
+
+    sp_mat L = INC * weighted_laplacian(k, m, dx, n, dy, alpha, beta) * ICN;
+
+    AddScalarBC::BC2D bc;
+    bc.dc = {1.0, 1.0, 1.0, 1.0};
+    bc.nc = {0.0, 0.0, 0.0, 0.0};
+    bc.v[0] = left(span(1, n));
+    bc.v[1] = right(span(1, n));
+    bc.v[2] = bottom;
+    bc.v[3] = top;
+
+    vec b = zeros(L.n_rows);
+
+    AddScalarBC::addScalarBC(L, b, k, m, dx, n, dy, bc);
+
+#ifdef EIGEN
+    vec sol = Utils::spsolve_eigen(L, b); 
+#else
+    vec sol = spsolve(L, b);
+#endif
+
+    return sol;
 }
 
-// 
-vec vacuum_grid()
+// Generates x- or y- coordinates of vacuum grid
+vec vacuum_grid(u16 k, u32 n, vec bottom, vec top)
 {
-    
+    Real alpha = 0.0;
+    Real beta = 0.0;
+
+    u32 m = top.n_elem;
+
+    Real dx = 1.0 / m;
+    Real dy = 1.0 / n;
+
+    ivec dc = {1, 1};
+    ivec nc = {0, 0};
+
+    Divergence De(k, m, dx, nc, nc); // Periodic in xi
+    Gradient Ge(k, m, dx, nc, nc);   // Periodic in xi
+
+    Divergence Dn(k, n, dy, dc, nc); // Nonperiodic in eta
+    Gradient Gn(k, n, dy, dc, nc);   // Nonperiodic in eta
+
+    sp_mat Im = speye(m, m);
+    sp_mat In = speye(n + 2, n + 2);
+    In.shed_col(0);
+    In.shed_col(n);
+
+    sp_mat Dx = Utils::spkron(In, De);
+    sp_mat Gx = Utils::spkron(In.t(), Ge);
+
+    sp_mat Dy = Utils::spkron(Dn, Im);
+    sp_mat Gy = Utils::spkron(Gn, Im); // Im.t() == Im
+
+    sp_mat A = (1.0 / (alpha * alpha)) * speye(Dx.n_cols, Gx.n_rows);
+    sp_mat B = (1.0 / (beta * beta)) * speye(Dy.n_cols, Gy.n_cols);
+
+    sp_mat L = Dx * A * Gx + Dy * B * Gy;
+
+    vec b = zeros(L.n_rows);
+
+    AddScalarBC::BC2D bc;
+    bc.dc = {0.0, 0.0, 1.0, 1.0};
+    bc.nc = {0.0, 0.0, 0.0, 0.0};
+    bc.v[0] = 0.0;
+    bc.v[1] = 0.0;
+    bc.v[2] = bottom;
+    bc.v[3] = top;
+
+    AddScalarBC::addScalarBC(L, b, k, m, dx, n, dy, bc);
+
+#ifdef EIGEN
+    vec sol = Utils::spsolve_eigen(L, b);
+#else
+    vec sol = spsolve(L, b);
+#endif
+
+    return sol;
+}
+
+// Reads coil information
+void read_coils()
+{
+
 }
--- src/cpp/tokamak.h
+++ src/cpp/tokamak.h
@@ -1,5 +1,9 @@
 /**
+ * @file tokamak.h
  * 
+ * @brief Functions for solving the Grad-Shafranov equation
+ * 
+ * @date 2026/4/8
  */
 
 #ifndef TOKAMAK_H
@@ -8,18 +12,56 @@
 #include "mole.h"
 
 /**
- * 
+ * @brief Path to file containing x-coordinates of tokamak boundary
  */
-sp_mat weighted_laplacian();
+#define XLIM_PATH ""
+
+/**
+ * @brief Path to file containing y-coordinates of tokamak boundary
+ */
+#define YLIM_PATH ""
+
+/**
+ * @brief Path to file containing coil information
+ */
+#define COIL_PATH ""
+
+/**
+ * @brief Constructs the laplacian matrix for u_xx / (alpha^2) + u_yy / (beta^2)
+ * 
+ * @param k Order of accuracy
+ * @param m Number of cells in x-direction
+ * @param dx Step size in x-direction
+ * @param n Number of cells in y-direction
+ * @param dy Step size in y-direction
+ * @param alpha parameter to scale x derivative
+ * @param beta parameter to scale y derivative
+ */
+sp_mat weighted_laplacian(u16 k, u32 m, Real dx, u32 n, Real dy, Real alpha, Real beta);
+
+/**
+ * @brief Constructs the x or y coordinates of the plasma domain using a reference grid
+ * 
+ * @param k Order of accuracy
+ * @param left coordinates of the left boundary, ordered bottom to top
+ * @param right coordinates of the right boundary, ordered bottom to top
+ * @param bottom coordinates of the bottom boundary, ordered left to right
+ * @param top coordinates of the top boundary, ordered left to right
+ */
+vec plasma_grid(u16 k, vec left, vec right, vec bottom, vec top);
+
+/**
+ * @brief Constructs to x- or y- coordinates of the vacuum domain using a reference grid
+ * 
+ * @param k Order of accuracy
+ * @param bottom coordinates of the plasma boundary, ordered left to right
+ * @param top coordinates of the tokamak boundary, ordered left to right
+ */
+vec vacuum_grid(u16 k, u32 n, vec bottom, vec top);
 
 /**
  * 
  */
-vec reference_grid();
-
-/**
- * 
- */
-vec vacuum_grid();
+void read_coils();
 
 #endif //TOKAMAK_H
