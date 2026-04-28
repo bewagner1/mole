fbf81a439a6c75b9b0b53b9f65f834813766856b
jbrzensk <jbrzensk@gmail.com>
2026-04-28 13:27:41 -0700

    changed functionarguments, smoother initial cond., and different error calculation

--- examples/matlab_octave/tutorials/two_way_wave_equation/comparison_two_way_wave_md_vs_fd.m
+++ examples/matlab_octave/tutorials/two_way_wave_equation/comparison_two_way_wave_md_vs_fd.m
@@ -100,13 +100,14 @@ k    = 2;        % Mimetic Order of Accuracy, can change to 4,6,8
 c    = 0.1;      % Velocity, 1 makes FD sceme exact
 west = -2.0;     % Domain's leftmost limits
 east = 2.0;      % Domain's rightmost limit
+dt   = 0.001;    % Time step, must work for all cell sizes
 
 % Number of cells to try, points is cells+1
-num_cells = [ 10, 20, 40, 80, 160 ];
+num_cells = [ 20, 40, 80, 160 ];
 
 %% Run each of the methods over the different grids
-[ U2_fd, error_fd, walltime_fd, flops_fd ] = finite_diff_two_way_wave_eq();
-[ U2_md, error_md, walltime_md, flops_md ] = mimetic_diff_two_way_wave_eq();
+[ U2_fd, error_fd, walltime_fd, flops_fd ] = finite_diff_two_way_wave_eq(c, dt, num_cells);
+[ U2_md, error_md, walltime_md, flops_md ] = mimetic_diff_two_way_wave_eq(k, c, dt, num_cells);
 
 %% Error analysis
 
